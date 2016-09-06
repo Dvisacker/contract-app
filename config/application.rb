9cd3bd84cf62913bfcccb39a11cb54c0c8f28aee
DAVID VAN ISACKER <davidvanisacker@gmail.com>
2016-09-06 18:55:11 +0900

    otni

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-config.serve_static_assets = true
 Bundler.require(*Rails.groups)
 
 
