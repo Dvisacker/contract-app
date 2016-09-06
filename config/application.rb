53eab6d26d53338b5e7aa00403ccdee91641d598
DAVID VAN ISACKER <davidvanisacker@gmail.com>
2016-09-06 18:47:07 +0900

    otni

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,9 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 config.serve_static_assets = true
+Bundler.require(*Rails.groups)
+
 
 
 module ContractApp
