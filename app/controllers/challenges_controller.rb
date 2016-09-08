3bd59c62562da65b6865d46284dd2285fa28be5c
DAVID VAN ISACKER <davidvanisacker@gmail.com>
2016-09-08 11:09:00 +0900

    ordered challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -4,7 +4,7 @@ class ChallengesController < ApplicationController
   # GET /challenges
   # GET /challenges.json
   def index
-    @challenges = Challenge.all
+    @challenges = Challenge.order('created_at DESC')
   end
 
   # GET /challenges/1
