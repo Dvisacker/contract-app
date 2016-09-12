7e7d2ba249348e90fb9aed938685d0d543b4579f
DAVID VAN ISACKER <davidvanisacker@gmail.com>
2016-09-12 18:30:03 +0900

    styled

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,10 +1,9 @@
-var ready;
+var ready; 
 
 ready = function(){
 	$('.chosen-select').chosen({
 		no_results_text: 'No results matched'
 	});
-}
 
+}
 $(document).ready(ready);
-$(document).on("page:load",read);
